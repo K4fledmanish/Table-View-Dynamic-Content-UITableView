625da46b1c28d3de7438022fe3093ff06f54619b
K4fledmanish <K4fledmanish@aol.com>
2020-09-14 15:01:43 +1000

    Initial Commit

---
+++ Table-View-Dynamic-Content-UITableView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Table-View-Dynamic-Content-UITableView
+//
+//  Created by K4fle D. Manish on 14/9/20.
+//  Copyright © 2020 K4fledmanish. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Table-View-Dynamic-Content-UITableView/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Table-View-Dynamic-Content-UITableView
+//
+//  Created by K4fle D. Manish on 14/9/20.
+//  Copyright © 2020 K4fledmanish. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
